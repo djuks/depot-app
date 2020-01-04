b80426dbc8e166e72eb6665193186a99b5262704
strahinja <djuricastrahinja@gmail.com>
2020-01-04 11:24:12 +0100

    Add Order mailer feature

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'webpacker', '~> 3.0'
 
+gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+gem 'draper'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'djurica96@gmail.com',
+    password:             'sifra123',
+    authentication:       'plain'
+    # enable_starttls_auto: true
+    # ^ ^ remove this option ^ ^
+  }
+end
